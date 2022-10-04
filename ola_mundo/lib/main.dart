ebd730dc5861e6b8805651c5ab78286b5af3d748
Ronald <ronaldcontact2019@gmail.com>
2022-10-04 19:50:51 -0300

    Textos no AppBar()

--- ola_mundo/lib/main.dart
+++ ola_mundo/lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 void main() => runApp(
   MaterialApp(
     home: Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
     )
   ),
 );
