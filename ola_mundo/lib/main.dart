d7cae6503c7596b7ce480bc3152e4a42c007d625
Ronald <ronaldcontact2019@gmail.com>
2022-10-04 20:39:13 -0300

    Acrescentando textos em coluna que está dentro de um card

--- ola_mundo/lib/main.dart
+++ ola_mundo/lib/main.dart
@@ -4,7 +4,14 @@ void main() => runApp(
   MaterialApp(
     home: Scaffold(
       body: Card(
-        child: Text('Ronald Santos'),
+        child: Column(
+          children: <Widget>[
+            Text('Ronald Santos 1'),
+            Text('Ronald Santos 2'),
+            Text('Ronald Santos 3'),
+            Text('Ronald Santos 4'),
+          ],
+        ),
       ),
       appBar: AppBar(
         title: Text('Transferências'),
