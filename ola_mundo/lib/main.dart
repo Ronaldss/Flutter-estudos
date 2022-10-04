8e69837925c897d9dfbad6038c14b0eb10fb8bfe
Ronald <ronaldcontact2019@gmail.com>
2022-10-04 19:26:08 -0300

    Estrutura inicial base - MaterialApp()
    Iniciando pequeno projeto representando uma transferencia bancária. Parte visual apenas.

--- ola_mundo/lib/main.dart
+++ ola_mundo/lib/main.dart
@@ -1,23 +1,6 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-  Column(
-    children:  <Widget>[
-      const Text('Ronald Santos', textDirection: TextDirection.ltr),
-      const Text('Curso de Flutter', textDirection: TextDirection.ltr),
-      const Text('Ifal', textDirection: TextDirection.ltr),
-      /*Expanded(
-        child: FittedBox(
-          fit: BoxFit.contain,
-          child: FlutterLogo(),
-          ),
-        ),teste*/
-        Column(
-          children: const<Widget>[
-            Text('Outro texto', textDirection: TextDirection.ltr),
-          ],
-        )
-    ],  
-  ),
+  MaterialApp(),
 );
 
